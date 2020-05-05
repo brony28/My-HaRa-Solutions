82c0d1105c87dac898dfc30707c0a0c108f725d5
Rony Benny <ronybenny2812@gmail.com>
2020-05-05 19:35:00 +0530

    itertools solution
    This tool returns  length subsequences of elements from the input iterable allowing individual elements to be repeated more than once.

---
+++ PYTHON/Itertools/itertools_combinations_with_replacements.py
@@ -0,0 +1,28 @@
+
+#ITERTOOLS.COMBINATIONS_WITH_REPLACEMENT()
+
+
+
+from itertools import combinations_with_replacement
+text,n = input().split()
+
+for i in combinations_with_replacement(sorted(text),int(n)):
+    print("".join(i))
+
+
+
+
+# itertools.combinations_with_replacement(iterable, r)
+# This tool returns  length subsequences of elements from the input iterable allowing individual elements to be repeated more than once.
+
+# Combinations are emitted in lexicographic sorted order. So, if the input iterable is sorted, the combination tuples will be produced in sorted order.
+
+# >>> from itertools import combinations_with_replacement
+# >>> 
+# >>> print list(combinations_with_replacement('12345',2))
+# [('1', '1'), ('1', '2'), ('1', '3'), ('1', '4'), ('1', '5'), ('2', '2'), ('2', '3'), ('2', '4'), ('2', '5'), ('3', '3'), ('3', '4'), ('3', '5'), ('4', '4'), ('4', '5'), ('5', '5')]
+# >>> 
+# >>> A = [1,1,3,3,3]
+# >>> print list(combinations(A,2))
+# [(1, 1), (1, 3), (1, 3), (1, 3), (1, 3), (1, 3), (1, 3), (3, 3), (3, 3), (3, 3)]
+
