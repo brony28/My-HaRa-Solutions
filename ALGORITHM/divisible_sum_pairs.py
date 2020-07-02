6f7e24ab9f92cc0928265301829c873f887859f4
Rony Benny <ronybenny2812@gmail.com>
2020-07-02 23:51:47 +0530

    Create divisible_sum_pairs.py
    Implementation subdomain

---
+++ ALGORITHM/divisible_sum_pairs.py
@@ -0,0 +1,35 @@
+#DIVISIBLE SUM PAIRS
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from itertools import combinations
+
+
+# Complete the divisibleSumPairs function below.
+def divisibleSumPairs(n, k, ar):
+    a = list(combinations(ar,2))
+    return sum((i[0]+i[1])%k==0 for i in a)
+    # return a
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
