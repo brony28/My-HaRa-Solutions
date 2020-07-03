2e9f10128b64cc4935d7efc14cfe9915217b7f3d
Rony Benny <ronybenny2812@gmail.com>
2020-07-03 14:26:38 +0530

    Create migratory_birds.py
    Implementation subdoman

---
+++ ALGORITHM/migratory_birds.py
@@ -0,0 +1,41 @@
+#MIGRATORY BIRDS
+
+
+#refered from Discussions... Complexity check(Y)
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import *
+
+# Complete the migratoryBirds function below.
+
+
+# class OrderedCounter(Counter,OrderedDict):
+#     pass
+
+def migratoryBirds(arr):
+    # a = OrderedCounter(arr)
+    # return a.most_common(1)[0][0]
+    count = [0]*6
+    for i in arr:
+        count[i]+=1
+    return count.index(max(count))
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
