5eaec21dea8906c0a3dc546c232937a08b4b9133
Rony Benny <ronybenny2812@gmail.com>
2020-07-21 03:49:27 +0530

    Create circular_array_rotation.py
    Implementation subdomain

---
+++ ALGORITHM/circular_array_rotation.py
@@ -0,0 +1,41 @@
+# CIRCULAR ARRAY ROTATION
+
+# #!/bin/python3
+
+# import math
+# import os
+# import random
+# import re
+# import sys
+# from collections import deque
+
+# def circularArrayRotation(a, k, queries):
+#     return [a[(i-k)%n] for i in queries]
+
+# if __name__ == '__main__':
+#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+#     nkq = input().split()
+#     n = int(nkq[0])
+#     k = int(nkq[1])
+#     q = int(nkq[2])
+#     a = list(map(int, input().rstrip().split()))
+#     queries = []
+#     for _ in range(q):
+#         queries_item = int(input())
+#         queries.append(queries_item)
+
+#     result = circularArrayRotation(a, k, queries)
+#     fptr.write('\n'.join(map(str, result)))
+#     fptr.write('\n')
+#     fptr.close()
+
+
+numOfEl,rotatnCount,numOfQuer = list(map(int,input().split()))
+a = list(map(int, input().split()))
+queries = []
+for _ in range(numOfQuer):
+    queries.append(int(input()))
+
+for i in queries:
+    print(a[(i-rotatnCount)%numOfEl],sep="\n")
+
