4720cd4ff0476c3a48550cc7900bfa69ae165ab5
Rony Benny <ronybenny2812@gmail.com>
2021-02-24 02:55:51 +0530

    day 3

--- JAVASCRIPT/day03_arrays.js
+++ JAVASCRIPT/day03_arrays.js
@@ -1,26 +1,26 @@
-'use strict';
+// 'use strict';
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+// process.stdin.resume();
+// process.stdin.setEncoding('utf-8');
 
-let inputString = '';
-let currentLine = 0;
+// let inputString = '';
+// let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
+// process.stdin.on('data', inputStdin => {
+//     inputString += inputStdin;
+// });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
+// process.stdin.on('end', _ => {
+//     inputString = inputString.trim().split('\n').map(string => {
+//         return string.trim();
+//     });
     
-    main();    
-});
+//     main();    
+// });
 
-function readLine() {
-    return inputString[currentLine++];
-}
+// function readLine() {
+//     return inputString[currentLine++];
+// }
 
 /**
 *   Return the second largest number in the array.
